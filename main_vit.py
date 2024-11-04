89e63a87c545c51c321e85790cb088b48ea9ffaa
Adyatma <awan2403@gmail.com>
2024-11-04 20:43:46 +0700

    main_vit update

--- main_vit.py
+++ main_vit.py
@@ -1,6 +1,8 @@
 import os
+import pandas as pd
 os.environ["CUDA_VISIBLE_DEVICES"]="0"
 os.environ["TF_GPU_ALLOCATOR"]="cuda_malloc_async"
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # 0 = all logs, 1 = warnings, 2 = errors
 import random
 import numpy as np
 import tensorflow as tf
@@ -24,7 +26,7 @@ def set_global_determinism(seed=SEED):
 
 set_global_determinism(seed=SEED)
 from tensorflow.keras.layers import CategoryEncoding
-from tf_keras.optimizers import Adam, RMSprop, SGD, Adadelta, Adagrad, Adamax, Nadam, Ftrl
+from tf_keras.optimizers import Adam, RMSprop, SGD, Adadelta, Adagrad, Adamax, Nadam, Ftrl, Lion
 from snn_vit import snn_vit
 import time
 from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix, f1_score
@@ -64,7 +66,11 @@ f.close()
 # y_val[y_val == 3] = 0
 # y_val[y_val == 4] = 1
 n_class = len(set(y_train))
-
+print("y_train ", n_class)
+n_class = len(set(y_val))
+print("y_val ", n_class)
+n_class = len(set(y_test))
+print("y_test ", n_class)
 # -
 
 if(n_class > 2):
@@ -93,25 +99,23 @@ def eval_cnn(predicted, y_test, n_class):
     rec = recall_score(label, prediction, average='weighted')
     confus = confusion_matrix(label, prediction)
 
-    return acc, fm, prec, rec, confus
+    return acc, fm, prec, rec, confus, prediction
 
 # opt_learn =  [Adam, RMSprop, SGD, Adadelta, Adagrad, Adamax, Nadam, Ftrl]
+# opt_learn = [Lion, RMSprop, SGD, Adam, Adagrad, Adadelta]
 # fm_ = -999
-# learn_rate = [0.0001,0.0005,0.001,0.005]
-
-# learn_batch = [512, 256, 128, 64, 32, 16, 8]
-
-
-
+# learn_rate = [0.0001,0.0005,0.001]
+# learn_batch = [512, 256, 128]
 
 fm_ = -999
 #Grid
 learn_rate = [0.001]
 learn_batch = [64]
-opt_learn =  [Adam]
+opt_learn =  [Lion, RMSprop, Adam]
 #learn_rate = [0.001]
 #learn_batch = [100]
 #opt_learn =  [SGD]
+
 for opt in opt_learn:
     for lr in learn_rate:
         for batch in learn_batch:
@@ -139,29 +143,66 @@ for opt in opt_learn:
             #------------------Training
             # predicted = model([X_train[:, 0], X_train[:, 1]], training = False)
             #
-            predicted = model.predict([X_test[:, 0], X_test[:, 1]], batch_size=16)
+            print("-------------------------------------------Training------------------------------------------")
+            predicted = model.predict([X_train[:, 0], X_train[:, 1]], batch_size=batch)
+            # predicted = model([X_train[:, 0], X_train[:, 1]], training = False)
 
-            acc, fm, prec, rec, confus = eval_cnn(predicted, y_train, n_class)
+            acc, fm, prec, rec, confus, prediction = eval_cnn(predicted, y_train, n_class)
+
+            print("Predicted: ", prediction)
+            print("Y_train: ", y_train)
+            print("n_class: ", n_class)
+            print()
 
             print("Accuracy: ", acc)
             print("F-Measure: ",fm)
             print("Precision: ",prec)
             print("Recall: ",rec)
             print(confus)
-
-
 
             #------------------Testing
-            print("--------------------------------------------------------------------------------")
-            predicted = model([X_test[:, 0], X_test[:, 1]], training = False)
+            print("-------------------------------------------Testing-------------------------------------------")
+            predicted = model.predict([X_test[:, 0], X_test[:, 1]], batch_size=batch)
+            # predicted = model([X_test[:, 0], X_test[:, 1]], training = False)
 
-            acc, fm, prec, rec, confus = eval_cnn(predicted, y_test, n_class)
+            acc, fm, prec, rec, confus, prediction = eval_cnn(predicted, y_test, n_class)
+
+            print("Predicted: ", prediction)
+            print("Y_test: ", y_test)
+            print("n_class: ", n_class)
+            print()
 
             print("Accuracy: ", acc)
             print("F-Measure: ",fm)
             print("Precision: ",prec)
             print("Recall: ",rec)
             print(confus)
+
+            # Append results to list
+            result_df = pd.DataFrame([{
+                "Optimizer": opt.__name__,
+                "Learning Rate": lr,
+                "Batch Size": batch,
+                "Accuracy": acc,
+                "F-Measure": fm,
+                "Precision": prec,
+                "Recall": rec,
+                "Confusion Matrix": confus.tolist()
+            }])
+
+
+            # Check if the results file exists
+            if os.path.exists("training_results.xlsx"):
+                # Read the existing Excel file
+                existing_df = pd.read_excel("training_results.xlsx")
+                # Append new results
+                updated_df = pd.concat([existing_df, result_df], ignore_index=True)
+            else:
+                # If the file does not exist, use the new results directly
+                updated_df = result_df
+
+            # Save the updated DataFrame back to the Excel file
+            updated_df.to_excel("training_results.xlsx", index=False)
 
 
             print("LR: ", lr, " Batch: ",batch," F-Measure test: ", fm)
@@ -172,8 +213,9 @@ for opt in opt_learn:
                 best_model = model
                 opt_ = opt
 
-
-
+# Save all results to an Excel file
+# results_df = pd.DataFrame(results)
+# results_df.to_excel("training_results.xlsx", index=False)
 
 print("Best learning_rate: ",best_lr)
 print("Best batch: ",best_batch)
@@ -182,41 +224,3 @@ print(best_model.summary())
 fm_ = str(fm_)
 fm_ = fm_[0:6]
 best_model.save('saved_model/'+str(fm_)+'_'+str(best_batch)+'_'+str(opt_)+'_lr_'+str(best_lr)+'_classweighted_blur16x16_.h5')
-
-# if __name__ == '__main__':
-#     import numpy as np
-#     import matplotlib.pyplot as plt
-#     import random
-#
-#     # Load the data
-#     with open('Data/3blur_all_data64x64.npy', 'rb') as f:
-#         X_train = np.load(f)
-#         y_train = np.load(f)
-#         X_val = np.load(f)
-#         y_val = np.load(f)
-#         X_test = np.load(f)
-#         y_test = np.load(f)
-#
-#     # Display shapes
-#     print("X_train shape:", X_train.shape)
-#     print("y_train shape:", y_train.shape)
-#     print("X_val shape:", X_val.shape)
-#     print("y_val shape:", y_val.shape)
-#     print("X_test shape:", X_test.shape)
-#     print("y_test shape:", y_test.shape)
-#
-#     # Select a random index
-#     random_index = random.randint(0, X_train.shape[0] - 1)
-#     print("Random index selected:", random_index)
-#
-#     # Extract the images from layer 0 (channel 0) and layer 2 (channel 1)
-#     layer_0_image = X_train[random_index, 0, :, :]
-#     layer_2_image = X_train[random_index, 1, :, :]
-#
-#     # Save the images to disk
-#     plt.imsave("layer_0_image.png", layer_0_image, cmap='gray')
-#     plt.imsave("layer_2_image.png", layer_2_image, cmap='gray')
-#
-#     print("Images saved as 'layer_0_image.png' and 'layer_2_image.png'.")
-
-
